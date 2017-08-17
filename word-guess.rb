54ab0a93828ac28cbcbd9c23e1b796162f6dd9f4
Canaan West <canaan.west@gmail.com>
2017-08-17 15:50:33 -0700

    many tests passing

--- word-guess.rb
+++ word-guess.rb
@@ -1,38 +1,122 @@
-class Game
+require "pry"
 
+class ASCII_display
+  attr_reader :ASCII_init, :displayed
   def initialize
-    @word_bank= []
-    @word= word_bank.sample
-    @spaces= @word.length
-    @bad_attempts= []
-    @picture
+    @ASCII_init = ["///", "////", "/////"]
+    @displayed = display_picture(@ASCII_init)
+    #store picture as an array
   end
 
-  def update_display
-    #replace a blank with a letter
-    #check indexes
-  end
-
-  def update_picture
-    #will call this on @display when the user incorrectly guesses letter
-  end
-
-  def check(guess)
-    if @word.include?(guess)
-      update_display # method that we will define later
-    else
-      update_picture
-      update_attempts # method we will define later
-  end
-
-
-  def update_attempts(guess)
-    @bad_attempts.push(guess)
-
-    if @bad_attempts.length == 6 #we can change default later
-      puts "You LOSE. #{@word} was the word."
+  def display_picture(picture)
+    picture.each do |line|
+      puts "#{line} \n"
     end
   end
 
+  def update_ASCII_display
+    @ASCII_init.delete(@ASCII_init[0])
+    return display_picture(@ASCII_init)
+  end
+end #End of ASCII_display class
+
+
+
+class Game
+  attr_reader :word, :word_bank, :word_array, :letters, :bad_attempts, :picture, :update_letters
+  def initialize
+    @word_bank= ["hello", "word"]
+    @word= @word_bank.sample
+    @word_array = @word.split("")
+    @letters = put_spaces(@word)
+    @bad_attempts= []
+    @picture = ASCII_display.new #SOME ASCII art as an arg
+  end
+
+#works!
+  def put_spaces(word)
+    spaces = []
+    word.length.times do |space|
+      spaces.push("_")
+    end
+    return spaces.join(" ")
+  end
+
+
+#will only excute when guess is correct
+#WORKS!
+  def update_letters(guess)
+    @word_array.each_index do |index|
+      if @word_array[index] == guess
+        @letters[index*2] = guess
+      end
+    end
+    return @letters
+  end
+
+
+
+  def update_picture
+    return @picture.update_ASCII_display
+  end
 
 end
+
+
+
+game1 = Game.new
+puts game1.letters
+puts game1.update_letters("o")
+puts game1.update_picture
+puts game1.update_picture
+
+
+
+#puts game1.picture.update_ASCII_display
+
+#
+#
+#   def update_bad_attempts(guess)
+#     @bad_attempts.push(guess)
+#
+#     if @bad_attempts.length == 6 #we can change default later
+#       puts "You LOSE. #{@word} was the word."
+#       exit
+#     else
+#       user_guesses
+#     end
+#   end
+#
+#
+# end
+#
+#
+#
+#
+
+
+
+#
+#
+# #~~~~~~~~~~
+#
+# game1 = Game.new
+#
+# def user_guesses
+#   puts "Pick a letter!!"
+#   guess = gets.chomp.downcase
+#   return guess
+# end
+#
+#
+# def check(guess)
+#   if game1.word.include?(guess) # does it need to be able to read the word?
+#     game1.update_letters # method that we will define later
+#   else
+#     game1.update_picture
+#     game1.update_bad_attempts # method we will define later
+#   end
+# end
+#
+#
+# check(user_guesses)
